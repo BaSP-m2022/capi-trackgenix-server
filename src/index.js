d83f31a447aa0ea5416b570ce637e2de8add54c2
Ivan <gobbi.ivandog@gmail.com>
2022-05-09 13:56:58 -0300

    TG: working on create admin progress submit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/admins', adminControllers.getAdminsAll);
+app.get('/admins/:id', adminControllers.getAdminById);
+app.post('/admins/create', adminControllers.createAdmin);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,12 +1,58 @@
-/* eslint-disable import/prefer-default-export */
+const fs = require('fs');
+// const req = require('express/lib/request');
 const admins = require('../data/admins.json');
 
+// admins.forEach((admin, index) => {
+//     console.log(admin.firstName , index);
+// })
+
+// get all admins
 const getAdminsAll = (req, res) => {
   res.status(200).json({
     data: admins,
   });
 };
 
-export {
+// get admin by ID
+function getAdminById(req, res) {
+  const { id } = req.params;
+  const admin = admins.find((a) => a.id === parseInt(id, 10));
+  if (admin) {
+    res.status(200).json(admin);
+  } else {
+    res.status(404).json({ msg: 'Admin not found' });
+  }
+}
+
+function createAdmin(req, res) {
+//   const {
+//     firstName, lastName, id, email, adminStatus, projects,
+//   } = req.body;
+//   if (firstName && lastName && id && email && adminStatus && projects) {
+  const adminNew = {
+    firstName: req.body.firstName || '',
+    lastName: req.body.lastName || '',
+    id: parseInt(req.body.id, 10),
+    email: req.body.email || '',
+    adminStatus: req.body.adminStatus || '',
+    projects: req.body.projects || '',
+  };
+  admins.push(adminNew);
+  fs.writeFile('./src/data/admins.json', JSON.stringify(admins));
+  res.status(201).json({ msg: 'Admin created', adminNew });
+//   } else {
+//     res.status(400).json({ msg: 'Error: Complete all data to create an admin' });
+//   }
+}
+
+module.exports = {
   getAdminsAll,
+  getAdminById,
+  createAdmin,
 };
+
+// Crear un Admin
+// Editar un Admin
+// Obtener un Admin
+// Eliminar un Admin
+// Obtener la lista de Admins con la opción de usar filtros
