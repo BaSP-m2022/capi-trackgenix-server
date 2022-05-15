068e94993198fa8b3f5d63b04d904b062c15d91b
Ivan <gobbi.ivandog@gmail.com>
2022-05-15 00:18:59 -0300

    TG: 'resources' folder changed for 'controllers' folder, 'routes' folder added with index.js and admins.js files, 'models' folder added with Admins.js file, 'validations' folder added with admins.js file.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes';
+import adminsRouter from './controllers/admins';
+import projectsRouter from './controllers/projects';
+import timeSheetsRouter from './controllers/time-sheets';
+import tasksRouter from './controllers/tasks';
+import employeesRouter from './controllers/employees';
+import superAdminRouter from './controllers/super-admins';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
